fa7003d4352a413997194ece230a8045279e3b8f
Iskhak Asanov <asanov.personal@gmail.com>
2022-12-13 19:39:30 +0600

    test

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,7 @@ const bot = new Bot(BOT_TOKEN);
 // attach all middleware
 bot.on("message", async (ctx) => {
   await ctx.reply("Hi there!");
+  await ctx.reply("Hi there! 2");
 });
 
 // The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
