46b739dfadc65e4c1b877b30290081c5e08c123b
Iskhak Asanov <asanov.personal@gmail.com>
2022-12-16 01:36:11 +0600

    localiztions resolved

--- api/index.ts
+++ api/index.ts
@@ -15,15 +15,8 @@ import {
   FULL_LANGUAGE_NAMES,
 } from "../constants/languages";
 import { I18n, I18nFlavor } from "@grammyjs/i18n";
-import directoryTree from "directory-tree";
 
 const BOT_TOKEN: string = process.env["BOT_TOKEN"] ?? "";
-
-const tree3 = directoryTree("./api");
-console.log("api directory", tree3);
-
-const tree2 = directoryTree(".");
-console.log("current directory", tree2);
 interface SessionData {
   __language_code?: string;
 }
@@ -32,8 +25,83 @@ type MyContext = Context & SessionFlavor<SessionData> & I18nFlavor;
 const i18n = new I18n<MyContext>({
   defaultLocale: "en",
   useSession: true, // whether to store user language in session
-  directory: "locales", // Load all translation files from locales/.
 });
+
+const loadLocalizations = async () => {
+  await i18n.loadLocale("en", {
+    source: `language = Language 
+    .english = English 
+    .french = French 
+    .german =  German  
+    .italian =  Italian  
+    .spanish =  Spanish  
+    .japanese =  Japanese  
+    .chinese =  Chinese  
+    .korean =  Korean  
+    .portuguese =  Portuguese  
+    .russian =  Russian  
+    .polish =  Polish  
+    .uzbek =  Uzbek  
+    .hindi =  Hindi  
+    .arabic =  Arabic  
+    .turkish =  Turkish 
+    .kyrgyz =  Kyrgyz 
+    .kazakh =  Kazakh 
+    .language-set =  Language set
+    .language-set-success =  Your language has been set to {$language}`,
+  });
+
+  await i18n.loadLocale("ky", {
+    source: `language = Тил 
+    .english = Аглисче 
+    .french = Французча 
+    .german =  Немисче  
+    .italian =  Италиянча  
+    .spanish =  Испанча  
+    .japanese =  Япончо  
+    .chinese =  Кытайча  
+    .korean = Корейче  
+    .portuguese = Португалча  
+    .russian = Орусча  
+    .polish = Полякча  
+    .uzbek =  Өзбекче
+    .hindi =  Индусча  
+    .arabic =  Арабча  
+    .turkish =  Түркчө
+    .kyrgyz =  Кыргызча 
+    .kazakh =  Казакча 
+    .language-set = Тил тандалды
+    .language-set-success =  Сиздин тандаган тилиңиз: {$language}
+    `,
+  });
+
+  await i18n.loadLocale("ru", {
+    source: `language = Язык 
+    .english = Английский 
+    .french = Французский 
+    .german =  Немецкий  
+    .italian =  Итальянский  
+    .spanish =  Испанский  
+    .japanese =  Японский  
+    .chinese =  Китайский  
+    .korean = Корейский  
+    .portuguese = Португальский  
+    .russian = Русский  
+    .polish = Польский  
+    .uzbek =  Узбекский  
+    .hindi =  Индуский  
+    .arabic =  Арабский  
+    .turkish =  Турецкий 
+    .kyrgyz =  Кыргызский 
+    .kazakh =  Казахский
+    .language-set = Язык выбран
+    .language-set-success =  Ваш язык был выбран: {$language}
+    `,
+  });
+};
+
+loadLocalizations();
+
 
 const URL =
   process.env["SUPABASE_URL"] ?? "https://zrzbwyqoeptuszgqmnin.supabase.co";
